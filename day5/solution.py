2994a2753e96db8766a6ed8f0543963efd901dd1
perezsaura-david <perezsaura.david@gmail.com>
2022-12-06 15:46:41 +0100

    Day5 solved

--- day5/solution.py
+++ day5/solution.py
@@ -1,8 +1,9 @@
 import os, argparse
+import numpy as np
 
 parser = argparse.ArgumentParser(description='')
 parser.add_argument('input', help='Input file to read from.')
-parser.add_argument('mode', help='')
+parser.add_argument('mode', help=' 1: single block move, 2: multiple block move')
 
 def getStorageInfo(file_line):
     row = []
@@ -21,19 +22,51 @@ def getStorageInfo(file_line):
             space_counter = 0
     return row
 
-def makeStructure(structure_info):
-    columns_id = structure_info[0]
+def getMoveInfo(file_line):
+
+    file_line = file_line.strip()
+    info = file_line.split(' ')
+    move = np.array(info[1::2], int) 
+
+    return move
+
+def makeStructure(structure):
+    columns_id = structure[0]
     
     columns = []
     for i in range(len(columns_id)):
         column = []
-        for row in reversed(structure_info[:-1]):
-            print(row)
+        for row in reversed(structure[:-1]):
             if row[i] != None:
                 column.append(row[i])
         columns.append(column)
 
-    print(columns)
+    return columns
+
+def moveBlocks(structure, move, mode):
+
+    quantity = move[0]
+    column_start = move[1] - 1
+    column_end = move[2] - 1
+
+    # print("Quantity: ", quantity)
+    # print("Column start: ", column_start)
+    # print("Column end: ", column_end)
+
+    if mode == 1:
+        for i in range(quantity):
+            block = structure[column_start][-1]
+            # print(f'block: {block}')
+            structure[column_start].pop()
+            structure[column_end].append(block)
+            # print(f'structure after block moved: {structure}')
+    if mode == 2:
+        blocks = structure[column_start][-quantity:]
+        # print(f'blocks: {blocks}')
+        structure[column_start] = structure[column_start][:-quantity]
+        structure[column_end] += blocks
+        # print(f'structure after blocks moved: {structure}')
+
     return
 
 
@@ -44,21 +77,36 @@ if __name__ == '__main__':
         print('Input file does not exist.')
         exit(1)
 
-    structure = []
+    structure_info = []
     reconstruction = True
     with open(args.input, 'r') as f:
         for line in f:
             if line == '\n':
                 reconstruction = False
-                # print(structure)
-                makeStructure(structure)
+                cargo_structure = makeStructure(structure_info)
+                continue
             if reconstruction:
-                structure.append(getStorageInfo(line))
+                structure_info.append(getStorageInfo(line))
+            else:
+                move = getMoveInfo(line)
+                moveBlocks(cargo_structure, move, int(args.mode))
 
     if args.mode == '1':
         print('Solution 1')
+        print(f'Final structure: {cargo_structure}')
+        # for column in cargo_structure:
+            # print(f'Column {column} last block: {column[-1]}')
+        last_blocks = ''
+        for column in cargo_structure:
+            last_blocks += column[-1]
+        print(f'Last blocks: {last_blocks}')
     elif args.mode == '2':
         print('Solution 2')
+        print(f'Final structure: {cargo_structure}')
+        last_blocks = ''
+        for column in cargo_structure:
+            last_blocks += column[-1]
+        print(f'Last blocks: {last_blocks}')
     else:
         print('Unknown mode.')
         exit(1)
